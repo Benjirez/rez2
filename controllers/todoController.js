3bd1fb24ed7196f10199ad81e764aebb18a316b3
benjirez <benjirez@gmail.com>
2018-02-02 17:00:59 -0800

    implemented ajax delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -8,6 +8,7 @@ module.exports = function(xsvr){
 	
 	xsvr.get('/todo', function(req, res){
 		res.render('todo', {todos: data});
+		console.log('todo called');
 	});
 	
 	xsvr.post('/todo', urlencodedParser, function(req, res){
@@ -15,8 +16,11 @@ module.exports = function(xsvr){
 		res.json(data);
 	});
 	
-	xsvr.delete('/todo/item', function(req, res){
-		
+	xsvr.delete('/todo/:item', function(req, res){
+		data = data.filter(function(todo){
+			return todo.item.replace(/ /g, '-') !== req.params.item;
+		});
+		res.json(data);
 	});
 	
 };
