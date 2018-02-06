4be82f2b7c2a3a918e1e8e69be97c7f7daaafdd7
benjirez <benjirez@gmail.com>
2018-02-06 09:25:07 -0800

    added mongoose, .git ignore dbcreds

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,29 @@
+var dbcreds = require('../dbcreds');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extend: false});
+console.log( dbcreds.pw );
+mongoose.connect('mongodb://'+ dbcreds.user +':' + dbcreds.pw + '@ds051913.mlab.com:51913/rezdb');
 
 var data = [{item: 'get blue milk'}, {item: "re-attach 3PO's arm"}, {item: 'pickup power converters at toshi station'}];
 
-var urlencodedParser = bodyParser.urlencoded({extend: false});
-
 module.exports = function(xsvr){
-	
+
 	xsvr.get('/todo', function(req, res){
 		res.render('todo', {todos: data});
 		console.log('todo called');
 	});
-	
+
 	xsvr.post('/todo', urlencodedParser, function(req, res){
 		data.push(req.body);
 		res.json(data);
 	});
-	
+
 	xsvr.delete('/todo/:item', function(req, res){
 		data = data.filter(function(todo){
 			return todo.item.replace(/ /g, '-') !== req.params.item;
 		});
 		res.json(data);
 	});
-	
+
 };
