ae7a77ab364f47793c8e0440f3fa4192e880bff0
benjirez <benjirez@gmail.com>
2018-02-08 09:16:03 -0800

    completed todo, still fumbles todos-with-hyphens

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,28 +2,38 @@ var dbcreds = require('../dbcreds');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extend: false});
-console.log( dbcreds.pw );
-mongoose.connect('mongodb://'+ dbcreds.user +':' + dbcreds.pw + '@ds051913.mlab.com:51913/rezdb');
 
-var data = [{item: 'get blue milk'}, {item: "re-attach 3PO's arm"}, {item: 'pickup power converters at toshi station'}];
+mongoose.connect('mongodb://'+ dbcreds.user +':' + dbcreds.pw + '@ds051913.mlab.com:51913/rezdb');
+var todoModel = mongoose.model('todo', mongoose.Schema({item: String}) );
+//var data = [{item: 'get blue milk'}, {item: "re-attach 3PO's arm"}, {item: 'pickup power converters at toshi station'}];
 
 module.exports = function(xsvr){
 
+	// get todo data and render view
 	xsvr.get('/todo', function(req, res){
-		res.render('todo', {todos: data});
+		var data = todoModel.find({}, function(err, data){
+			if (err) throw err;
+			//console.log('into the shoot flyboy');
+			res.render('todo', {todos: data});
+		});
+
 		console.log('todo called');
 	});
 
+	//create new todoModel with data from req.body, push to db, reload view
 	xsvr.post('/todo', urlencodedParser, function(req, res){
-		data.push(req.body);
-		res.json(data);
+		var newTodo = todoModel(req.body).save(function(err, data){
+			if (err) throw err;
+			res.json(data);
+		});
 	});
 
 	xsvr.delete('/todo/:item', function(req, res){
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g, '-') !== req.params.item;
+		//delete requested item from db
+		todoModel.find( {item: req.params.item.replace(/\-/g, " ")} ).remove(function(err, data){
+			if (err) throw err;
+			res.json(data);
 		});
-		res.json(data);
 	});
 
 };
