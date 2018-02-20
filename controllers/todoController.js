d3b6d5682a59f9a9f4134f3dbd4a79b7168781ee
benjirez <benjirez@gmail.com>
2018-02-20 15:09:00 -0800

    trimmed fat, made 2 columns

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,13 +4,13 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extend: false});
 
 mongoose.connect('mongodb://'+ dbcreds.user +':' + dbcreds.pw + '@ds051913.mlab.com:51913/rezdb');
-var todoModel = mongoose.model('todo', mongoose.Schema({item: String}) );
+var todoModel = mongoose.model('rez2_collection2', mongoose.Schema({item: String}) );
 //var data = [{item: 'get blue milk'}, {item: "re-attach 3PO's arm"}, {item: 'pickup power converters at toshi station'}];
 
 module.exports = function(xsvr){
 
 	// get todo data and render view
-	xsvr.get('/todo', function(req, res){
+	xsvr.get('/', function(req, res){
 		var data = todoModel.find({}, function(err, data){
 			if (err) throw err;
 			//console.log('into the shoot flyboy');
